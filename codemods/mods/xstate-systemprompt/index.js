7922a2ad14d00695da78cb7c7b97306d75559104
Ryan Bennett <nomad.ry@gmail.com>
2025-03-20 11:38:23 -0600

    add model selection via xstate

--- codemods/mods/xstate-systemprompt/index.js
+++ codemods/mods/xstate-systemprompt/index.js
@@ -3,6 +3,7 @@ const path = require("path")
 
 // Import transforms
 const transformSystemTs = require("./transforms/xstate-systemprompt")
+const transformClineTs = require("./transforms/xstate-model")
 
 async function apply(rootDir) {
 	console.log("Applying XState systemprompt integration codemod...")
@@ -15,12 +16,22 @@ async function apply(rootDir) {
 
 async function applyTransforms(rootDir) {
 	// Apply transform to system.ts file
-	const systemTsFile = path.join(rootDir, "src/core/Cline.ts")
+	// Apply transform to Cline.ts file
+	const clineTsFile = path.join(rootDir, "src/core/Cline.ts")
 
-	if (await fs.pathExists(systemTsFile)) {
-		const content = await fs.readFile(systemTsFile, "utf8")
+	if (await fs.pathExists(clineTsFile)) {
+		const content = await fs.readFile(clineTsFile, "utf8")
 		const transformed = transformSystemTs(content)
-		await fs.writeFile(systemTsFile, transformed)
+		await fs.writeFile(clineTsFile, transformed)
+		console.log("Transformed: src/core/Cline.ts")
+	} else {
+		console.error("Could not find src/core/Cline.ts")
+	}
+
+	if (await fs.pathExists(clineTsFile)) {
+		const content = await fs.readFile(clineTsFile, "utf8")
+		const transformed = transformClineTs(content)
+		await fs.writeFile(clineTsFile, transformed)
 		console.log("Transformed: src/core/Cline.ts")
 	} else {
 		console.error("Could not find src/core/Cline.ts")
