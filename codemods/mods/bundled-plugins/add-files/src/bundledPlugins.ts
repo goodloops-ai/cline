59ca0df17f97b5a49e1b27abfffad26d18c99da5
Ryan Bennett <nomad.ry@gmail.com>
2025-04-24 16:17:42 -0600

    update xstate

--- codemods/mods/bundled-plugins/add-files/src/bundledPlugins.ts
+++ codemods/mods/bundled-plugins/add-files/src/bundledPlugins.ts
@@ -4,13 +4,6 @@ export const bundledPlugins = [
 		githubUrl: "https://github.com/goodloops-ai/xstate-mcp.git",
 		entrypoint: "src/index.js",
 		command: "node",
-		checkout: "6d90f2757d4fa5502977dbbd88243e1c1f182a2f",
-	},
-	{
-		name: "shell-mcp",
-		githubUrl: "https://github.com/goodloops-ai/shell-mcp.git",
-		entrypoint: "server.js",
-		command: "node",
-		checkout: "eea93545b5ffccb4f41902b138c597f52c0dc653",
+		checkout: "cf962c5df1f22206d3c9bb03008910437c5d2a30",
 	},
 ]
